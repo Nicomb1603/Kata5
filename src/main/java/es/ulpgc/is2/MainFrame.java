8756c36df00fb6a8ef418f3d77d74d3b7884f6bc
Nicolás Marcelo Batista <nicolasmb1603@gmail.com>
2024-11-30 16:52:20 +0000

    2.1.1

--- src/main/java/es/ulpgc/is2/MainFrame.java
+++ src/main/java/es/ulpgc/is2/MainFrame.java
@@ -1,7 +1,6 @@
 package es.ulpgc.is2;
 
 import es.ulpgc.is2.control.LoadRandomUserCommand;
-import es.ulpgc.is2.view.ImageVisualizer;
 import es.ulpgc.is2.view.SwingImageVisualizer;
 
 import javax.swing.*;
@@ -9,20 +8,29 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     LoadRandomUserCommand command;
+    SwingImageVisualizer imageVisualizer;
+
     public MainFrame(){
-        SwingImageVisualizer imageVisualizer = new SwingImageVisualizer();
-        command = new LoadRandomUserCommand(1);
-        command.execute();
+        imageVisualizer = new SwingImageVisualizer();
+        getRandomUser();
         this.setTitle("MainFrame");
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-        imageVisualizer.show(command.getUsers().getFirst().getPhoto());
+        showPhoto();
         this.add(imageVisualizer, BorderLayout.CENTER);
 
     }
 
+    private void showPhoto() {
+        imageVisualizer.show(command.getUsers().getFirst().getPhoto());
+    }
+
+    private void getRandomUser() {
+        command = new LoadRandomUserCommand(1);
+        command.execute();
+    }
 
 
 }
